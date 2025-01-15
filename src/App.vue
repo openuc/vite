09d5933d9294bd433da5c636d821065b24acec62
OpenUC Name <45517795+openuc@users.noreply.github.com>
2025-01-15 23:43:25 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue + Dev Server Hook 123456" />
+  <HelloWorld msg="Vite + Vue + Dev Server Hook 1234567" />
 </template>
 
 <style scoped>
